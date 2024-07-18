866de0e8b3a03d94303bf2883c6623f5e619eed5
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-18 23:06:19 +0545

    group in middleware

--- routes/web.php
+++ routes/web.php
@@ -7,28 +7,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route ::group(['prefix'=>'/account'],function (){
 
-    Route::group (['middleware'=>'guest'],function(){
-        Route::get('/login', [loginController::class, 'index'])->name('login.page');
-        Route::get('/register', [loginController::class, 'register'])->name('register.page');
-        Route ::post('/register', [loginController::class, 'processRegister'])->name('register');
-        Route ::post('/login', [loginController::class, 'authenticate'])->name('account.login');
-        Route::get('/logout', [loginController::class, 'logout'])->name('account.logout');
-    });
-
-    
-    Route::group (['middleware'=>'auth'],function(){
-        
-    // Route::post('/login/all', [loginController::class, 'authenticate'])->name('account.login');
-
-    Route::get('dashboard', [DashboardController::class, 'index'])->name('home');
-    });
-});
-// Route::get('/account/login', [loginController::class, 'index'])->name('login.page');
-// Route::get('/account/register', [loginController::class, 'register'])->name('register.page');
-// Route ::post('/account/register', [loginController::class, 'processRegister'])->name('register');
-// Route ::get('/account/louut', [loginController::class, 'processRegister'])->name('account.logout');
-// Route::get('/account/logout', [loginController::class, 'logout'])->name('account.logout');
+Route::get('/account/login', [loginController::class, 'index'])->name('login.page');
+Route::get('/account/register', [loginController::class, 'register'])->name('register.page');
+Route ::post('/account/register', [loginController::class, 'processRegister'])->name('register');
+Route ::get('/account/louut', [loginController::class, 'processRegister'])->name('account.logout');
+Route::get('/account/logout', [loginController::class, 'logout'])->name('account.logout');
 
 
+
+Route::post('/login/all', [loginController::class, 'authenticate'])->name('account.login');
+
+
+// for groupin the routes
+Route ::group (['middleware' => 'auth'], function(){
+    Route::get('login/dashboard', [DashboardController::class, 'index'])->name('home');
+}); 
+// Route::get('login/dashboard', [DashboardController::class, 'index'])->name('home');
