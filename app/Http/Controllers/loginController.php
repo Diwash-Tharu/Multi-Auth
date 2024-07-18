a16128c7e4dd56aa5a7f30cfe9d7ef8a6bd2df0c
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-18 22:01:09 +0545

    redirect to login page

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -1,6 +1,10 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 use Illuminate\Http\Request;
 
@@ -10,4 +14,72 @@ class loginController extends Controller
     public function index(){
         return view('login');
     }
+
+    // this method will aunthicate the use 
+    public function authenticate(Request $request)
+    {
+        $validatoe = validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if($validatoe->passes()){
+            
+            if(Auth::attempt(['email'=>$request->email,'password'=>$request->password])){
+                // return redirect()->route('home');
+                return redirect()->route('home');
+            }
+            else{
+                return redirect()->route('login.page')->with('error','Invalid email or password');
+            }
+            // return redirect()->back()->withErrors($validatoe)->withInput();
+        }
+        else{
+            // return view('login');
+            return redirect()->route('login.page')
+            ->withErrors($validatoe)
+            ->withInput();
+        }
+
+    }
+
+    public function register(){
+        // Auth::logout();
+        // return redirect()->route('login.page');
+        return view('register');
+    }
+
+    public function processRegister(Request $request)
+    {
+        $validatoe = validator::make($request->all(),[
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required',
+        ]);
+
+        if($validatoe->passes()){
+            
+            $user = new User();
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password);
+            $user->role='customer';
+            $user->save();
+            
+            return redirect()->route('login.page')->with('success','Account created successfully');
+            // return redirect()->back()->withErrors($validatoe)->withInput();
+        }
+        else{
+            // return view('login');
+            return redirect()->route('register.page')
+            ->withErrors($validatoe)
+            ->withInput();
+        }
+    }
+
+    public function logout(){
+        Auth::logout();
+        return redirect()->route('login.page');
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\loginController;
+use App\Http\Controllers\DashboardController;	
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/login', [loginController::class, 'index'])->name('login.page');
+Route::get('/register', [loginController::class, 'register'])->name('register.page');
+Route ::post('/account/register', [loginController::class, 'processRegister'])->name('register');
+Route ::get('/account/louut', [loginController::class, 'processRegister'])->name('account.logout');
+
+
+
+Route::post('/login/all', [loginController::class, 'authenticate'])->name('account.login');
+
+Route::get('login/dashboard', [DashboardController::class, 'index'])->name('home');
