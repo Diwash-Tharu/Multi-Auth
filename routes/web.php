5ca424f7a8744a638e764ee5d5122429731ae8be
Diwash Tharu <diwash.tharu.ch@gmail.com>
2024-07-18 22:43:28 +0545

    group in the web

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,21 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login', [loginController::class, 'index'])->name('login.page');
-Route::get('/register', [loginController::class, 'register'])->name('register.page');
-Route ::post('/account/register', [loginController::class, 'processRegister'])->name('register');
-Route ::get('/account/louut', [loginController::class, 'processRegister'])->name('account.logout');
-Route::get('/account/logout', [loginController::class, 'logout'])->name('account.logout');
+Route ::group(['prefix'=>'account'],function (){
+
+    Route::group (['middleware'=>'guest'],function(){
+        Route::get('/login', [loginController::class, 'index'])->name('login.page');
+        Route::get('/register', [loginController::class, 'register'])->name('register.page');
+        Route ::post('/register', [loginController::class, 'processRegister'])->name('register');
+        Route ::post('/login', [loginController::class, 'authenticate'])->name('account.login');
+        Route::get('/logout', [loginController::class, 'logout'])->name('account.logout');
+    });
+// });
+// Route::get('/account/login', [loginController::class, 'index'])->name('login.page');
+// Route::get('/account/register', [loginController::class, 'register'])->name('register.page');
+// Route ::post('/account/register', [loginController::class, 'processRegister'])->name('register');
+// Route ::get('/account/louut', [loginController::class, 'processRegister'])->name('account.logout');
+// Route::get('/account/logout', [loginController::class, 'logout'])->name('account.logout');
 
 
 
